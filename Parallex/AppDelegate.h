9fde69270ab1ddb2e24b3d28d08f70a1e2f9aefb
tringgopi <gopinath.e@tringapps.com>
2015-06-30 14:42:09 +0530

    Initial Commit

---
+++ Parallex/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Parallex
+//
+//  Created by Gopinath Ezhumalai on 30/06/15.
+//  Copyright (c) 2015 Gopinath Ezhumalai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Parallex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Parallex
+//
+//  Created by Gopinath Ezhumalai on 30/06/15.
+//  Copyright (c) 2015 Gopinath Ezhumalai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
