c7608b30ab953dd97a13c8298d792b9ce374bbe5
tringgopi <gopinath.e@tringapps.com>
2015-07-01 17:30:23 +0530

    Parallex Commit

--- Parallex/ViewController.m
+++ Parallex/ViewController.m
@@ -18,7 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+UIScrollView
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
